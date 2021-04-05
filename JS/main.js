11722fad310178e5141579da65ca8ed335c948eb
JorgeMatDev <jorgemat.dev@gmail.com>
2021-04-05 19:23:53 -0300

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+(function() {
+
+    var carouselImgs = new Carousel ({
+        container: '.slide-show .main-section-cards',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+
+})()
