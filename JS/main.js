23e2e3fea9fa414621bd180b82474c3d13e104ec
JorgeMatDev <jorgemat.dev@gmail.com>
2021-04-06 09:58:30 -0300

    Scroll-menu-add

--- JS/main.js
+++ JS/main.js
@@ -8,3 +8,12 @@
     })
 
 })()
+
+window.addEventListener('scroll', function() {
+    let $_header = document.querySelector('.header-main');
+    let $_menu = document.querySelector('.menu');
+    let windowPosition = window.scrollY > 0;
+
+    $_header.classList.toggle('scroll-menu', windowPosition);
+    $_menu.classList.toggle('scroll-menu', windowPosition)
+})
